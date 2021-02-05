f85869918866928f4b2c56c1a5a092e5bdabded4
github username <github email address>
2021-02-04 19:52:24 -0500

    commit tree fecfa2861b6b4b59681f4e6833d773d1ce5c8448

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!\n"
+        number -= 1
+    end
+    return "HAPPY NEW YEAR!" 
+
+end
+
+def countdown_with_sleep(int)
+
+    while int > 0
+        sleep int
+        puts "#{int} SECOND(S)!\n"
+        int -= 1
+    end
+    return "HAPPY NEW YEAR!" 
+
 end
